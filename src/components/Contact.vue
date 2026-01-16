025d5c2607a63316c1333c7609214927a72cb03c
Siva Ponniah <siva.ponniah@hva.nl>
2026-01-16 20:11:36 +0100

    wip

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -6,17 +6,17 @@
         <p class="text-xl my-4">Get in touch with me via linkedin or by phone.</p>
         <div class="grid grid-cols-2 items-center">
           <div  class="flex items-center">
-            <img src="@/assets/phone-call.png" alt="Gmail" class="h-10 w-10" />
+            <img src="/assets/phone-call.png" alt="Gmail" class="h-10 w-10" />
             <p class="ml-2 text-xl">0639747147</p>
           </div>
           <a href="https://www.linkedin.com/in/siva-ponniah-832421257/" class="flex items-center">
-            <img src="@/assets/linkedin.png" alt="LinkedIn" class="h-16 w-16"/>
+            <img src="/assets/linkedin.png" alt="LinkedIn" class="h-16 w-16"/>
             <p class="text-xl text-Linkedin sm:hover:text-Linkedin">LinkedIn</p>
           </a>
         </div>
       </div>
       <div class="md:ml-10 mt-10 md:mt-0">
-        <img src="@/assets/3779143.jpg" alt="Profile Art" class="rounded-full border-2 border-orange-500"/>
+        <img src="/assets/3779143.jpg" alt="Profile Art" class="rounded-full border-2 border-orange-500"/>
       </div>
     </div>
   </div>
